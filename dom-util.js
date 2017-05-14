2defc2e980d17f0ccbee30192eb4ac95e7952990
Yuki Omori <yuki.omori@inte.co.jp>
2017-05-14 18:37:42 +0900

    implemented visitAllTrees and flattenTreeToArray

--- dom-util.js
+++ dom-util.js
@@ -1,12 +1,20 @@
 
 const visitAllNodes = function(node, callback) {
-  // Hint: read about DOM nodes and node methods here: https://developer.mozilla.org/en-US/docs/Web/API/Node
-  // Your code here
+  if (node.childNodes.length > 0) {
+    for (let i = 0; i < node.childNodes.length; i++) {
+      visitAllNodes(node.childNodes[i], callback);
+    }
+  }
+  callback(node);
 };
 
 const flattenTreeToArray = function(node) {
   // Hint: Use visitAllNodes()
-  // Your code here
+  let flattenArray = [];
+  visitAllNodes(node, () => {
+    flattenArray.push(node.size);
+  });
+  return flattenArray;
 };
 
 module.exports = {
