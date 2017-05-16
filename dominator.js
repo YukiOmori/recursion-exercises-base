24b54ede0b61b6df5bd8b3d0971bcd8ad035265b
Yuki Omori <yuki.omori@inte.co.jp>
2017-05-16 20:53:49 +0900

    implemented dominator

--- dominator.js
+++ dominator.js
@@ -2,23 +2,48 @@ const { flattenTreeToArray } = require('./dom-util');
 const _ = require('underscore');
 
 const getElementById = function(root, id) {
-  let element = {};
+  let element = null;
   let flatRoot = flattenTreeToArray(root);
-  _.each(flatRoot, function (idTag) {
-    if (root.id === id) {
-      element.id === id;
-      element.textContent = root.textContent;      
-    } 
+  _.each(flatRoot, (node) => {
+    if (node.id === id) {
+      element = node;
+    }
   });
   return element;
 };
 
+
+const containClassName = function (classNames, className) {
+  let classNameExist = false;
+  _.each(classNames, (value) => {
+    classNameExist = classNameExist || value === className;
+  });
+  return classNameExist;
+};
+
 const getElementsByClassName = function(root, className) {
-  // Your code here
+  let elements = [];
+  let flatRoot = flattenTreeToArray(root);
+  _.each(flatRoot, (node) => {
+    if (node.className !== undefined) {
+      let classNames = node.className.split(' ');    
+      if (containClassName(classNames, className)) {
+        elements.push(node);
+      }
+    }
+  });
+  return elements;
 };
 
 const getElementsByTagName = function(root, tagName) {
-  // Your code here
+  let elements = [];
+  let flatRoot = flattenTreeToArray(root);
+  _.each(flatRoot, (node) => {
+    if (node.tagName === tagName) {
+      elements.push(node);
+    }
+  });
+  return elements;
 };
 
 module.exports = {
